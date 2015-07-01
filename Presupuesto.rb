31af9b8002e90908d46c33185a2766faf1f0d1df
Croelan Grandez <croelanjr@cbs-corporation.com.pe>
2015-07-01 00:10:20 -0500

    actualizado

---
+++ Presupuesto.rb
@@ -0,0 +1,52 @@
+=begin
+Caso 4 : Una empresa de arquitectura está implementando un 
+sistema de presupuestos automáticos. Para ello requiere 
+que la presentación muestra un mensaje de esta forma:
+
+Carlos Perez, el presupuesto calculado es: 2983.12
+
+Este mensaje debe ser mostrado en una sola cadena. 
+
+Para ello se pide crear una clase cuyos atributos sean el nombre 
+de la persona y el monto a pagar. 
+
+TIPS : 
+Se puede investigar la conversión de valores flotantes a 
+cadenas (string) aquí:
+http://ruby-doc.org/core-2.1.2/Float.html
+
+
+Tener en cuenta que los valores de la velocidad son números enteros.
+=end
+
+class Presupuesto
+
+	attr_accessor :nombre_cliente, :monto_presupuesto
+
+ def initialize(nombre_cliente, monto_presupuesto)
+ 	@nombre_cliente = nombre_cliente
+ 	@monto_presupuesto = monto_presupuesto
+ end
+
+ def mostrar
+ 	return nombre_cliente + ', el presupuesto calculado es : ' + monto_presupuesto.to_s
+ end 
+
+end
+
+presupuesto01 = Presupuesto.new("Miguel Grau", 2342.23)
+presupuesto02 = Presupuesto.new("Francisco Bolognesi", 3442.25)
+presupuesto03 = Presupuesto.new("Chavin de Huantar", 5542.30)
+presupuesto04 = Presupuesto.new("Alfonso Ugarte", 65462.25)
+presupuesto05 = Presupuesto.new("Perricholi Garcia", 5542.56)
+presupuesto06 = Presupuesto.new("Atahualpa Tupayachi", 8565.53)
+
+
+puts "Lista de Presupuesto de clientes"
+puts "--------------------------------"
+puts presupuesto01.mostrar
+puts presupuesto02.mostrar
+puts presupuesto03.mostrar
+puts presupuesto04.mostrar
+puts presupuesto05.mostrar
+puts presupuesto06.mostrar
